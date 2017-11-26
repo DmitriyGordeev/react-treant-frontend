cef0f2059d7ed7d941c5e0f78848a129b7cf8c93
Weekend Master <weekend@super.com>
2017-11-26 19:33:52 +0300

    deps

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'treant-js';
+import 'treant-js/Treant.css';
+import 'treant-js/vendor/raphael';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.body);
+
