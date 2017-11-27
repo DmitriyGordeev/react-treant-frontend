088c2f7bdb6aeddfa9dd97230f9b3d4f79c2632d
Weekend Master <weekend@super.com>
2017-11-27 13:22:29 +0300

    some example

--- src/app/index.js
+++ src/app/index.js
@@ -4,23 +4,104 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const simple_chart_config = {
+var chart_config = {
     chart: {
-        container: "#tree-simple"
-    },
+        container: "#tree-simple",
 
+        connectors: {
+            type: 'step'
+        },
+        node: {
+            HTMLclass: 'nodeExample1'
+        }
+    },
     nodeStructure: {
-        text: { name: "Parent node" },
+        text: {
+            name: "Mark Hill",
+            title: "Chief executive officer",
+            contact: "Tel: 01 213 123 134",
+        },
+        // image: "../headshots/2.jpg",
         children: [
             {
-                text: { name: "First child" }
+                text:{
+                    name: "Joe Linux",
+                    title: "Chief Technology Officer",
+                },
+                stackChildren: true,
+                // image: "../headshots/1.jpg",
+                children: [
+                    {
+                        text:{
+                            name: "Ron Blomquist",
+                            title: "Chief Information Security Officer"
+                        },
+                        // image: "../headshots/8.jpg"
+                    },
+                    {
+                        text:{
+                            name: "Michael Rubin",
+                            title: "Chief Innovation Officer",
+                            contact: "we@aregreat.com"
+                        },
+                        // image: "../headshots/9.jpg"
+                    }
+                ]
             },
             {
-                text: { name: "Second child" }
+                stackChildren: true,
+                text:{
+                    name: "Linda May",
+                    title: "Chief Business Officer",
+                },
+                // image: "../headshots/5.jpg",
+                children: [
+                    {
+                        text:{
+                            name: "Alice Lopez",
+                            title: "Chief Communications Officer"
+                        },
+                        // image: "../headshots/7.jpg"
+                    },
+                    {
+                        text:{
+                            name: "Mary Johnson",
+                            title: "Chief Brand Officer"
+                        },
+                        // image: "../headshots/4.jpg"
+                    },
+                    {
+                        text:{
+                            name: "Kirk Douglas",
+                            title: "Chief Business Development Officer"
+                        },
+                        // image: "../headshots/11.jpg"
+                    }
+                ]
+            },
+            {
+                text:{
+                    name: "John Green",
+                    title: "Chief accounting officer",
+                    contact: "Tel: 01 213 123 134",
+                },
+                // image: "../headshots/6.jpg",
+                children: [
+                    {
+                        text:{
+                            name: "Erica Reel",
+                            title: "Chief Customer Officer"
+                        },
+                        link: {
+                            href: "http://www.google.com"
+                        },
+                        // image: "../headshots/10.jpg"
+                    }
+                ]
             }
         ]
     }
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
-var my_chart = new Treant(simple_chart_config);
+var my_chart = new Treant(chart_config);
