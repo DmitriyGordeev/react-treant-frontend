984de02098667de634c1e6f75f198da32178910f
Weekend Master <weekend@super.com>
2017-11-29 17:31:51 +0300

    changed redux state

--- src/app/nodeFuncs.js
+++ src/app/nodeFuncs.js
@@ -24,8 +24,25 @@
 //     });
 // }
 
+import tree from './tree';
+import ReduxState from './reduxState';
+
 function onNodeClickEvent() {
-    alert(this.innerHTML);
+    var nodeObject = {
+        HTMLid: "0",
+        innerHTML:
+        "<p class='node-name'>Start</p>" +
+        "<i class=\"material-icons node-button\">add</i>",
+        connectors: {
+            style: {
+                'stroke': '#bbb',
+                'arrow-end': 'oval-wide-long'
+            }
+        },
+        children: []
+    };
+    tree.treeData.nodeStructure.children.push(nodeObject);
+    console.log("redux state:", ReduxState.treeData.nodeStructure.children.length);
 }
 
 export default function onNodeClick() {
