4353586d292347f229e3cfcd29bc8df1b724fd26
Weekend Master <weekend@super.com>
2017-11-29 18:34:02 +0300

    removed unused comment

--- src/app/nodeFuncs.js
+++ src/app/nodeFuncs.js
@@ -1,28 +1,4 @@
 
-// export default function nodeClick() {
-//     jQuery(".big-commpany .node-button").click(function() {
-//         var clickedNode = jQuery(this).closest(".big-commpany");
-//         var clickedNode_id = clickedNode.attr("id");
-//         var node = {
-//             HTMLid: "0",
-//             innerHTML:
-//             "<p class='node-name'>Hey, fuck off!</p>" +
-//             "<i class=\"material-icons node-button\">add</i>",
-//             connectors: {
-//                 style: {
-//                     'stroke': '#bbb',
-//                     'arrow-end': 'oval-wide-long'
-//                 }
-//             },
-//             children: [],
-//         };
-//
-//         tree.addNode(clickedNode_id, node);
-//
-//
-//         console.log(tree.treeData);
-//     });
-// }
 
 import tree from './tree';
 import ReduxStore from './reduxStore';
