0ae9e6b54179f0f70dd26e1ef582b49e12de89ba
Weekend Master <weekend@super.com>
2017-12-02 21:34:06 +0300

    separate TreantContainer

--- src/app/App.js
+++ src/app/App.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import './App.css';
-
-
-function hasClass(elem, className) {
-    return elem.className.toString().split(' ').indexOf(className) > -1;
-}
+import TreantContainer from './TreantContainer.js';
 
 class App extends React.Component {
 
-    onTreeContainerClick(event) {
-        if(event.target !== null) {
-            if (hasClass(event.target, 'node-button')) {
-                var nodeId = event.target.parentNode.getAttribute("id");
-                this.props.onNodeClickDispatcher(nodeId);
-            }
-        }
-    }
-
     render() {
         return (
-            <div id={"tree-container"} onClick={this.onTreeContainerClick.bind(this)}> </div>
-        );
+            <TreantContainer/>
+        )
     }
 }
-
 
 export default connect(
     state => ({
         storeData: state
     }),
-    dispatch => ({
-        onNodeClickDispatcher: (clicked_id) => {
-            dispatch({ type: 'NODE_CLICK', nodeId: clicked_id })
-        }
-    })
+    dispatch => ({})
 )(App);
-
---
+++ src/app/TreantContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import './TreantContainer.css';
+
+
+function hasClass(elem, className) {
+    return elem.className.toString().split(' ').indexOf(className) > -1;
+}
+
+class TreantContainer extends React.Component {
+
+    onTreeContainerClick(event) {
+        if(event.target !== null) {
+            if (hasClass(event.target, 'node-button')) {
+                var nodeId = event.target.parentNode.getAttribute("id");
+                this.props.onNodeClickDispatcher(nodeId);
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div id={"tree-container"} onClick={this.onTreeContainerClick.bind(this)}> </div>
+        );
+    }
+}
+
+
+export default connect(
+    state => ({
+        storeData: state
+    }),
+    dispatch => ({
+        onNodeClickDispatcher: (clicked_id) => {
+            dispatch({ type: 'NODE_CLICK', nodeId: clicked_id })
+        }
+    })
+)(TreantContainer);
+
