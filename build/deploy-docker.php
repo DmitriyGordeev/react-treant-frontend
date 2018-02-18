d76d7832dee2e47defca2dabc12e2d0392431592
Weekend Master <weekend@super.com>
2018-02-18 21:19:01 +0300

    some fixes ?

--- build/deploy-docker.php
+++ build/deploy-docker.php
@@ -2,4 +2,6 @@
 $user_id = "1";
 $docker_build_cmd = "docker build -t ".$user_id."-bot-worker .";
 $docker_run_bot_cmd = "docker run -d ".$user_id."-bot-worker";
-exec($docker_build_cmd." && ".$docker_run_bot_cmd);
+$result = exec($docker_build_cmd." && ".$docker_run_bot_cmd);
+
+// error_log($result, 3, "php-logs/docker-build.log");
